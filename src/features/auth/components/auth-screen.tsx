f1d6da40c26b97412f09faf51bab2fc4d24ab3d7
Salar Shabahang <karishab86@gmail.com>
2025-01-09 23:48:24 +0100

    insallation

---
+++ src/features/auth/components/auth-screen.tsx
@@ -0,0 +1,14 @@
+"use cleint";
+
+// import { useState } from "react";
+
+// import { SignInFlow } from "../types";
+
+export const AuthScreen = () => {
+    // const [state, setState] = useState<SignInFlow>("signIn");
+    return (
+        <div className="h-full flex items-center justify-center bg-[#d0ad10]">
+            auth Screen
+        </div>
+    );
+};
