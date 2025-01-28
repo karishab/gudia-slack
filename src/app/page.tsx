a1e7370723ef6d8dd4fe25562f0241210c2bbcf5
Salar Shabahang <karishab86@gmail.com>
2025-01-28 12:27:37 +0100

    sign in und sign out with github fnished -01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-
-
+"use client";
+import { Button } from "@/components/ui/button";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
+  const { signOut } = useAuthActions();
   return (
     <div>
-      Loggin !
+      Logged In !
+      <Button onClick={() => signOut()}>
+        Sign out
+      </Button>
     </div>
   );
 }
